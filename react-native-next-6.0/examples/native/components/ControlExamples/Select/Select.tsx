b400ff2ba9a8b41146dcfc0059d52b404f4b364a
ManTT-12 <74489008+ManTT-12@users.noreply.github.com>
2021-11-14 10:27:56 +0700

    Delete react-native-next-6.0 directory

--- react-native-next-6.0/examples/native/components/ControlExamples/Select/Select.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-interface Props {
-  arrow: string;
-}
-
-export const SelectExample = ({ arrow }: Props) => <Text>Selected: {arrow}</Text>;
--- react-native-next-6.0/examples/native/components/ControlExamples/Text/Text.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-export const Heading = ({ text }: { text: string }) => <Text>{text}</Text>;
